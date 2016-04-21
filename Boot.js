141fec2543b5a53cde3d0b0a44d39c0b9f29878e
Chase Wikberg <Chasewikberg@gmail.com>
2016-04-21 11:55:57 -0400

    Coding and creating sprites.

--- Boot.js
+++ Boot.js
@@ -1,1 +1,24 @@
 var DesolateStorm = {};
+
+DesolateStorm.Boot = function(game) {};
+
+DesolateStorm.Boot.prototype = {
+	
+	preload: function() {},
+
+	create: function() {
+		this.input.maxPointers = 1;
+		this.stage.disableVisibilityChange = false;
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.minWidth = 540;
+		this.scale.minHeight = 480;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.stage.forcePortrait = true;  
+		this.scale.setScreenSize(true); 
+
+		this.input.addPointer();
+		this.stage.backgroundColor = '#202123';
+	}
+	
+};
