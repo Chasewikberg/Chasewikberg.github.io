b043a1cde521d29f2545af8a979574d16c065fc4
Chase Wikberg <Chasewikberg@gmail.com>
2016-04-28 14:33:13 -0400

    More coding and changing images around.

--- Preloader.js
+++ Preloader.js
@@ -7,22 +7,23 @@ DesolateStorm.Preloader = function(game) {
 DesolateStorm.Preloader.prototype = {
 	
 	preload: function () {
-        this.load.image('titlescreen','');
-        this.load.atlasXML('adultBedroom','bedroom_sprite.png','bedroomsprite.xml')
-        this.load.atlasXML('entrance','chandelier_sprite.png','chandeliersprite.xml') 
-        this.load.audio('metalclang', 'clinksound.mp3')
-        this.load.image('deadGuy','deadguy.png')
-        this.load.image('hallWay1','Hallway1.png')
-        this.load.image('hallWay2','Hallway2.png')
-        this.load.image('kid','Kid.gif')
-        this.load.atlasXML('kidsBedroom', 'Kids_bedroom_sprite.png','Kids_bedroom ')
-        this.load.atlasXML('kitchen','kitchen_sprite.png','kitchensprite.xml')
-        this.load.image('leadPipe','Lead_Pipe.png')
-        this.load.atlasXML('livingroom','livingroom_sprite.png','livingroomsprite.xml')
-        this.load.image('policeRadio','Police_Radio.png')
-        this.load.atlasXML('policeOfficer','Policeofficersprite.png','Policeofficersprite.xml')
-        this.load.image('speechBubble','Speech_bubble.png')
-        this.loud.sound('thunderSound', 'thundersound.mp3')
+        this.load.atlasXML('adultBedroom','bedroom_sprite.png','bedroomsprite.xml');
+        this.load.atlasXML('entrance','chandelier_sprite.png','chandeliersprite.xml') ;
+        this.load.audio('metalclang', 'clinksound.mp3');
+        this.load.image('deadGuy','Dead_guy.png');
+        this.load.image('hallWay1','Hallway1.png');
+        this.load.image('hallWay2','Hallway2.png');
+        this.load.image('kid','Kid.gif');
+        this.load.atlasXML('kidsBedroom', 'Kids_bedroom_sprite.png','Kids_bedroom_sprite.xml');
+        this.load.atlasXML('kitchen','kitchen_sprite.png','kitchensprite.xml');
+        this.load.image('leadPipe','Lead_Pipe.png');
+        this.load.atlasXML('livingroom','livingroom_sprite.png','livingroomsprite.xml');
+        this.load.image('policeRadio','Police_Radio.png');
+        this.load.atlasXML('policeOfficer','Policeofficersprite.png','Policeofficersprite.xml');
+        this.load.bitmapFont('scaryfont','', '');
+        this.load.image('speechBubble','Speech_bubble.png');
+        this.load.audio('thunderSound','thundersound.mp3');
+        this.load.image('titleScreen','Title_screen.png');
     },
 
 	create: function () { 
--- Startmenu.js
+++ Startmenu.js
@@ -1,20 +1,20 @@
-BunnyDefender.StartMenu = function (game) {
+DesolateStorm.StartMenu = function (game) {
     this.startBG;
     this.startPrompt;
 };
 DesolateStorm.StartMenu.prototype = {
 	
 	create: function () {
-        this.ding = this.add.audio('thunderSound');
-		startBG = this.add.image(0, 0, 'titlescreen');
+        this.thundermusic = this.add.audio('thunderSound');
+        this.thundermusic.play();
+		startBG = this.add.image(0, 0, 'titleScreen');
 		startBG.inputEnabled = true;
 		startBG.events.onInputDown.addOnce(this.startGame, this);
 		
-		startPrompt = this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'eightbitwonder', 'Touch to Start!', 24);
+		startPrompt = this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'scarytext', 'Face Your Fears!', 24);
 	},
 
 	startGame: function (pointer) {
-        this.ding.play();
 		this.state.start('Game');
 	}
 };
