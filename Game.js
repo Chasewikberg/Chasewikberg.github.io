e0a38f19556d86bbdbdd224be12b2b55ca522719
Chase Wikberg <Chasewikberg@gmail.com>
2016-05-16 14:31:21 -0400

    Changes to game

--- Game.js
+++ Game.js
@@ -1,15 +1,22 @@
-DesolateStorm.game = function (game) {
+DesolateStorm.Game = function (game) {
     
     
 };
 
 DesolateStorm.Game.prototype = {
-    create: function () {
+    create: function () { 
         
+        this.openingSequence();
+    },
+    openingSequence () {
+        this.stage.backgroundColor = '#000000';
+        this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'scaryFont', '', 24);
         
+        this.buildWorld
     },
     
     buildWorld: function () {
         this.add.image(0,720,'policeOfficer')
     },
+    
 }
