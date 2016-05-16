1af78639a1ddb61816c282cfb280133c5e56182d
Chase Wikberg <Chasewikberg@gmail.com>
2016-05-16 13:58:04 -0400

    Starting the Game

---
+++ Game.js
@@ -0,0 +1,15 @@
+DesolateStorm.game = function (game) {
+    
+    
+};
+
+DesolateStorm.Game.prototype = {
+    create: function () {
+        
+        
+    },
+    
+    buildWorld: function () {
+        this.add.image(0,720,'policeOfficer')
+    },
+}
