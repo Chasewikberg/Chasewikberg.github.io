989ce75fc542c0234f8941c889b678802b25e121
tvtim2016 <tvtim2011@gmail.com>
2016-05-20 13:43:06 -0400

    ?

--- Game.js
+++ Game.js
@@ -12,11 +12,11 @@ DesolateStorm.Game.prototype = {
         this.stage.backgroundColor = '#000000';
         this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'scaryFont', 'IV I V PR at V III VI Fulton Street. This was the call that changed everything.', 24);
         
-        this.buildWorld
+        this.buildWorld()
     },
     
     buildWorld: function () {
-        this.add.image(0,720,'policeOfficer')
+        this.load.atlasXML('entrance','chandelier_sprite.png','chandeliersprite.xml') ;
     },
     
 }
