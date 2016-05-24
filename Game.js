aa528dae34cc3280d666b30f99eba8b28da6f49c
Chase Wikberg <Chasewikberg@gmail.com>
2016-05-24 14:28:04 -0400

    Centered fonts

--- Game.js
+++ Game.js
@@ -10,8 +10,8 @@ DesolateStorm.Game.prototype = {
     },
     openingSequence () {
         this.stage.backgroundColor = '#000000';
-        this.add.bitmapText(this.world.centerX - 135, this.world.centerY + 140, 'scaryFont', 'Four fifteen PR at five thirty six Fulton Street.', 24);
-        this.add.bitmapText(this.world.centerX - 135, this.world.centerY + 160, 'scaryFont', 'This was the call that changed everything.', 24);
+        this.add.bitmapText(this.world.centerX - 295, this.world.centerY - 130, 'scaryFont', 'Four fifteen PR at five thirty six Fulton Street.', 24);
+        this.add.bitmapText(this.world.centerX - 250, this.world.centerY - 100, 'scaryFont', 'This was the call that changed everything.', 24);
         
         
         this.buildWorld()
