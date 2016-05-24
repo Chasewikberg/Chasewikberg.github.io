3d17b48d545c4ddab3bcc8cdfa101de8381efd8c
tvtim2016 <tvtim2011@gmail.com>
2016-05-24 14:12:23 -0400

    ?

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-Desolate Storm.Game = function (game) {
+DesolateStorm.Game = function (game) {
     
     
 };
@@ -10,13 +10,15 @@ DesolateStorm.Game.prototype = {
     },
     openingSequence () {
         this.stage.backgroundColor = '#000000';
-        this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'scaryFont', 'IV I V PR at V III VI Fulton Street. This was the call that changed everything.', 24);
+        this.add.bitmapText(this.world.centerX - 145, this.world.centerY + 160, 'scaryFont', 'Four fifteen PR at five thirty six Fulton Street.', 24);
+        this.add.bitmapText(this.world.centerX - 145, this.world.centerY + 180, 'scaryFont', 'This was the call that changed everything.', 24);
+        
         
         this.buildWorld()
     },
     
     buildWorld: function () {
-        this.load.atlasXML('entrance','chandelier_sprite.png','chandeliersprite.xml') ;
+
     },
     
 }
